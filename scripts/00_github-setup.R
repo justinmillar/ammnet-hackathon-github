4639f3783c76c5cb56f2e3662d79abf9b9f9f5c1
Justin Millar <justinjmillar@gmail.com>
2026-01-14 08:58:32 -0500

    Created a new script for connected git creds to RStudio.

---
+++ scripts/00_github-setup.R
@@ -0,0 +1,20 @@
+# Linking Github to RStudio
+# 1. Make sure our git and Github information is connected to RStudio
+# 2. Practice pushing new commits
+# 3. Push an interactive graph to Github
+
+# Connect Git and Github information to RStudio
+# install.packages("usethis")
+library(usethis)
+
+git_sitrep()
+
+# Pass git creds to RStudio by creating a Github token
+usethis::gh_token_help()
+usethis::create_github_token()
+
+git_sitrep()
+
+
+# Try making a local commit
+
